f893031ef2d7deff82ad8b09ef9d38a794b72292
xuechao <xuechao@e-ling.com.cn>
2017-10-20 18:24:04 +0800

    修改

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -1,39 +1,50 @@
 <template>
-  <div>
+  <div class="search">
     <search
       :results="results"
       v-model="searchValue"
       position="absolute"
       @on-submit="searchDate"
       ref="search"></search>
-
+      <panel  :list="list"></panel>
   </div>
 </template>
 
 <script>
-  import { Search } from 'vux'
+  import { Search, Panel } from 'vux'
   import axios from 'common/js/axios'
 
   export default {
     components: {
-      Search
+      Search,
+      Panel
     },
     data () {
       return {
         results: [],
-        searchValue: ''
+        searchValue: '',
+        list: []
       }
     },
     methods: {
       searchDate: function (data) {
         console.log(this.searchValue)
+        let that = this
         axios.get('/search', {
           params: {
             keyword: data
           }
         }).then((res) => {
-          if (res.data.code === 1) {
-            console.log(res.data)
+          if (res.data.code === 1 && res.data.data.books) {
+            let books = res.data.data.books
+            for (let i = 0; i < books.length; i++) {
+              that.list.push({
+                src: 'http://statics.zhuishushenqi.com' + books[i].cover,
+                title: books[i].title,
+                desc: books[i].shortIntro,
+                url: ''
+              })
+            }
           }
         })
       }
@@ -42,5 +53,8 @@
 </script>
 
 <style>
-
+  .search{
+    height: 100%;
+    overflow-y: auto;
+  }
 </style>
